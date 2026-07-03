7800a3c57f1dab7ccf1a4bad5d2e3a14e73dca86
iki1uc <entertrainer@web.de>
2026-07-04 00:16:09 +0200

    Update pook.js

--- pook.js
+++ pook.js
@@ -1,1 +1,10 @@
+export const POOK = {
+  mode: "narrativ",
+  behavior: "erzählt",
+  active: false,
+
+  story(respo){
+    return `POOK erzählt: ${respo} befindet sich im narrativen Zustand.`;
+  }
+};
 
